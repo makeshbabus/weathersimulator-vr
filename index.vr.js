1b1b45e1c4f2ffe7326396e7c174cfdb5462b23c
makeshbabu <makeshbabus@gmail.com>
2021-01-27 15:50:47 +0530

    fetching data from API

--- index.vr.js
+++ index.vr.js
@@ -4,8 +4,22 @@ import { View, Text, Pano, AppRegistry, asset, VrAnimated } from 'react-vr';
 const api_key = "d6b214d96073dceff7b10962989323fd";
 
 class WeatherSimulator extends Component {
-  render(){
-    return(
+  constructor() {
+    super();
+
+    this.state = {
+      weatherObject: {}
+    }
+  }
+  componentDidMount() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => this.setState({ weatherObject: json }));
+  }
+  render() {
+    return (
       <View>
         <Pano source={asset('lombard-vr.jpg')}></Pano>
       </View>
@@ -13,4 +27,4 @@ class WeatherSimulator extends Component {
   }
 }
 
-AppRegistry.registerComponent('WeatherSimulator', ()=>WeatherSimulator);
+AppRegistry.registerComponent('WeatherSimulator', () => WeatherSimulator);
